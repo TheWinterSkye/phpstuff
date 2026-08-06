31056107075448753ff68f627e364a25eac3c86e
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2026-08-06 01:43:13 -0500

    Add Keizaal Voice Shifter entry point

---
+++ keizaal-voice-shifter/src/main.cpp
@@ -0,0 +1,8 @@
+#include "common.inc"
+#include "audio.inc"
+#include "ui.inc"
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int) {
+    VoiceApp app(instance);
+    return app.Run();
+}
